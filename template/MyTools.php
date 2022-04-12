f6885c674c2f2177a53185b3a8fe109747c24456
Jaikt Dev <jaikrishna.t@geektechtips.xyz>
2022-04-12 18:05:39 +0530

    rename, del fake file

--- template/MyTools.php
+++ template/MyTools.php
@@ -5,6 +5,11 @@
 				<h1 class="page-title">Free Useful Tools</h1>
 			</div>
 		</div>
+		<div class="alert alert-danger bg-red py-4" id="fundingNotice" style="display: block;">
+				<div class="text-center">
+					<h3>This is a dummy placeholder where you should insert your ad code. <br>Insert the ad code below this <kbd>div</kbd> element. Best of luck!</h3>
+				</div>
+			</div>
 		<div class="row mt-2">
 			<div class="col-md-3 col-lg-3 mb-3">
 				<div class="card text-center pt-2 pb-2 m-20">
@@ -15,57 +20,12 @@
 					</a>
 				</div>
 			</div>
-			<div class="col-md-3 col-lg-3 mb-3">
-				<div class="card text-center pt-2 pb-2">
-					<div class="ribbon bg-orange">Updated</div>
-					<a href="privacyPolicy.php">
-						<img src="https://img.icons8.com/color/100/000000/privacy.png">
-						<h2>Privacy Policy</h2>
-					</a>
-				</div>
-			</div>
-			<div class="col-md-3 col-lg-3 mb-3">
-				<div class="card text-center pt-2 pb-2">
-					<a href="minifyCSS.php">
-						<img src="https://img.icons8.com/color/100/000000/css3.png">
-						<h2>CSS Minifier</h2>
-					</a>
-				</div>
-			</div>
-			<div class="col-md-3 col-lg-3 mb-3">
-				<div class="card text-center pt-2 pb-2">
-					<a href="beautifyCSS.php">
-						<img src="https://img.icons8.com/color/100/000000/violet-flower.png">
-						<h2>CSS Beautifier</h2>
-					</a>
-				</div>
-			</div>
-			<div class="col-md-3 col-lg-3 mb-3">
-				<div class="card text-center pt-2 pb-2">
-					<a href="phpEncoder.php">
-						<img src="https://img.icons8.com/color/100/000000/code.png">
-						<h2>PHP Encoder</h2>
-					</a>
-				</div>
-			</div>
-			<div class="col-md-3 col-lg-3 mb-3">
-				<div class="card text-center pt-2 pb-2">
-					<a href="xmlTojson.php">
-						<img src="https://img.icons8.com/stickers/100/000000/placeholder-thumbnail-json-1.png">
-						<h2>XML To Json</h2>
-					</a>
-
-				</div>
-			</div>
-			<div class="col-md-3 col-lg-3 mb-3">
-				<div class="card text-center pt-2 pb-2">
-					<div class="ribbon bg-green">New</div>
-					<a href="domainAGE.php">
-						<img src="https://img.icons8.com/color/100/000000/age.png">
-						<h2>Domain Age Checker</h2>
-					</a>
-				</div>
-			</div>
 		</div>
+		<div class="alert alert-danger bg-red py-4" id="fundingNotice" style="display: block;">
+				<div class="text-center">
+					<h3>This is a dummy placeholder where you should insert your ad code. <br>Insert the ad code below this <kbd>div</kbd> element. Best of luck!</h3>
+				</div>
+			</div>
 	</div>
+</div>
 </div>
