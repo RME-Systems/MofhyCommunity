e964537ab9f08a3aecbbec26a6b12a23de074d55
Jaikt Dev <jaikrishna.t@geektechtips.xyz>
2022-04-12 16:36:07 +0530

    change to prep stmt

--- function/MyPassword.php
+++ function/MyPassword.php
@@ -3,45 +3,25 @@ include __DIR__.'/Connect.php';
 require __DIR__.'/../handler/CookieHandler.php';
 if(isset($_POST['submit'])){
 	$FormData = array(
-		'old_password' => $_POST['old_password'],
-		'new_password' => $_POST['new_password'],
 		'hashed_password' => hash('sha256', $_POST['new_password']),
 		'user_key' => $ClientInfo['hosting_client_key'],
-		'user_password' => $ClientInfo['hosting_client_password'],
 	);
-	if(hash('sha256', $FormData['old_password']) == $FormData['user_password']){
-		$sql = mysqli_query($connect,"UPDATE `hosting_clients` SET `hosting_client_password`='".$FormData['hashed_password']."' WHERE `hosting_client_key`='".$FormData['user_key']."'");
-		if($sql){
-			$_SESSION['message'] = '<div class="alert alert-success" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Password changed <b>successfully!</b>
-									</div>';
-			setcookie('LEFSESS','NULL',-1,'/');
-			header('location: ../login.php');
-		}
-		else{
-			$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Something went'."'".'s <b>wrong!</b>
-									</div>';
-			header('location: ../mysettings.php');
-		}
+	$sql = "UPDATE `hosting_clients` SET `hosting_client_password`= ? WHERE `hosting_client_key`= ?";
+	$stmt = $connect->prepare($sql);
+	$stmt->bind_param("ss", $FormData['hashed_password'], $FormData['user_key']);
+	$trigger = $stmt->execute();
+	$stmt->close();
+	if($trigger !== false){
+		$_SESSION['message'] = '<div class="alert alert-success" role="alert">Your password has been changed!</div>';
+		setcookie('LEFSESS', 'NULL', -1, '/');
+		header('location: ../login');
 	}
 	else{
-		$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Invalid user <b>password!</b>
-									</div>';
-		header('location: ../mysettings.php');
+		$_SESSION['message'] = '<div class="alert alert-danger"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-x alert-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <circle cx="12" cy="12" r="9"></circle> <path d="M10 10l4 4m0 -4l-4 4"></path></svg>&nbsp;Yikes, something is going the wrong way. Please contact support. Error: '.$stmt->error.'</div>';
+		header('location: ../userProfile');
 	}
-}
-else{
-	header('location: ../');
-}
+	}
+	else{
+		header('location: ../');
+	}
 ?>
