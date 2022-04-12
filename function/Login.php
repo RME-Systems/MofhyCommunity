4db19d1637279cfe13321ccd57611280ff338810
Jaikt Dev <jaikrishna.t@geektechtips.xyz>
2022-04-12 12:50:44 +0530

    change to prepared stmt

--- function/Login.php
+++ function/Login.php
@@ -2,51 +2,26 @@
 require __DIR__.'/Connect.php';
 if(isset($_POST['login'])){
 	$FormData = array(
-		'email' => mysqli_real_escape_string($connect, $_POST['email']),
-		'password' => mysqli_real_escape_string($connect, $_POST['password'])
+		'email' => filter_var($_POST['email'], FILTER_VALIDATE_EMAIL),
+		'password' => hash('sha256', mysqli_real_escape_string($connect, $_POST['password'])),
+		'error_msg' => '<div class="alert alert-danger">These credentials do not match our records.</div>'
 	);
-	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_email`='".$FormData['email']."'");
-	if(mysqli_num_rows($sql)>0){
-		$Data = mysqli_fetch_assoc($sql);
-		if(trim($Data['hosting_client_password'])==hash('sha256', $FormData['password'])){
-			$key = rand(000000,999999);
-			$token = hash('sha256', json_encode([$FormData['email'], $Data['hosting_client_key'], $key]));
-			if(isset($_POST['remember'])){
-				setcookie('LEFSESS', base64_encode(gzcompress(json_encode(['email' => $FormData['email'],'token' => $token,'key' => $key]))), time()+30+86400, '/');
-			}
-			else{
-				setcookie('LEFSESS', base64_encode(gzcompress(json_encode(['email' => $Data['hosting_client_email'],'token' => $token,'key' => $key]))), time()+86400, '/');
-			}
-			$_SESSION['message'] = '<div class="alert alert-success" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Logged in <b>successfully!</b>
-									</div>';
-			header('location: ../index.php');
-		}
-		else{
-			$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Invalid <b>email address </b>or<b> password!</b>
-									</div>';
-			header('location: ../login.php');
-		}
-
+	$sql = "SELECT * FROM `hosting_clients` WHERE `hosting_client_email`= ? AND `hosting_client_password`= ? LIMIT 1";
+	$stmt = $connect->prepare($sql);
+	$stmt->bind_param("ss", $FormData['email'], $FormData['password']);
+	$stmt->execute();
+	$stmt_result = $stmt->get_result();
+	$stmt->close();
+	if(!$stmt_result->num_rows>0){
+		$_SESSION['message'] = $FormData['error_msg'];
+		header('location: ../login');
 	}
 	else{
-		$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Account doesn'."'".'t <b>exsist!</b>
-									</div>';
-		header('location: ../login.php');
+		setcookie('LEFSESS', bin2hex($FormData['email']), time()+3600, '/');
+		header('location: ../');
 	}
 }
 else{
-	header('location: ../login.php');
+	header('location: ../login');
 }
 ?>
--- handler/CookieHandler.php
+++ handler/CookieHandler.php
@@ -1,42 +1,17 @@
 <?php
 if(isset($_COOKIE['LEFSESS'])){
-	$data = json_decode(gzuncompress(base64_decode($_COOKIE['LEFSESS'])), true);
-	$token = $data['token'];
-	$email = $data['email'];
-	$key = $data['key'];
-	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_email`='".$email."'");
+	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_email`='".hex2bin($_COOKIE['LEFSESS'])."'");
 	if(mysqli_num_rows($sql)>0){
-		$ClientInfo = mysqli_fetch_assoc($sql);
-		$verify = hash('sha256', json_encode([$email, $ClientInfo['hosting_client_key'], $key]));
-		if(trim($token) !== trim($verify)){
-			setcookie('LEFSESS', null ,-1,'/');
-			$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-										  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-										    <span aria-hidden="true">&times;</span>
-										  </button>
-										  Login to <b>continue!</b>
-										</div>';
-			header('location: login.php');
-		}
+		$ClientInfo = mysqli_fetch_Assoc($sql);
 	}
 	else{
-		setcookie('LEFSESS', null ,-1,'/');
-		$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Login to <b>continue!</b>
-									</div>';
+		setcookie('LEFSESS',NULL,-1,'/');
+		$_SESSION['message'] = '<div class="alert alert-danger">Your session has been expired.</div>';
 		header('location: login.php');
 	}
 }
 else{
-	$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
-									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
-									    <span aria-hidden="true">&times;</span>
-									  </button>
-									  Login to <b>continue!</b>
-									</div>';
+	$_SESSION['message'] = '<div class="alert alert-danger">Your session has been expired.</div>';
 	header('location: login.php');
 }
 ?>
